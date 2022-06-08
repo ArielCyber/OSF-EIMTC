28f8b2e470e7ee7b40e075487d5845d7e50f7e55
neyney10 <neyney10@live.com>
2022-06-08 20:37:01 +0300

    fix bug in model preprocessing where the function wasnt static.

--- src/EIMTC/preprocessing/_distiller.py
+++ src/EIMTC/preprocessing/_distiller.py
@@ -1,5 +1,6 @@
         
 class DistillerPreprocessing:
+    @staticmethod
     def preprocess_features(dataframe):
         from EIMTC.plugins.n_bytes import NBytes
         from EIMTC.plugins.protocol_header_fields import ProtocolHeaderFields
