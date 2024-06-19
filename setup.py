b24119ece1916117c9d99767890932839f12979f
Adi Lichy <adi_lichy@yahoo.com>
2024-06-19 11:04:22 +0300

    update extractor

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf8")
 
 setup(
     name='OSF_EIMTC',
-    version='0.1.56',
+    version='0.1.57',
     description='A Framework for Encrypted Internet and Malicious Traffic Classification.',
     long_description=long_description,
     long_description_content_type='text/markdown',
